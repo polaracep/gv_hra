9055851db4172899bb833431ac67520f7cce6e96
Polarace <adam@beyer.cz>
2025-02-07 23:30:23 +0100

    Working cmake

--- main.cpp
+++ main.cpp
@@ -13,12 +13,34 @@
   windrealm.org
 */
 
-#include <SDL/SDL.h>
 #include <grafika/grafika.h>
-#include <stdio.h>
 
-int main()
+int main(int argc, char** argv)
 {
-    printf("Hello, world!");
-    return 0;
+    Obrazovka* obrazovka = Obrazovka::instance();
+    obrazovka->inicializuj(800, 600, 0, 0);
+
+    while (1) {
+        obrazovka->smaz();
+
+        /* zacatek kresleni */
+        barva(BILA);
+        cara(0, 0, 100, 100);
+
+        /* konec kresleni */
+        obrazovka->aktualizuj();
+
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym) {
+                case SDLK_ESCAPE:
+                    SDL_Quit();
+                    return 0;
+                }
+                break;
+            }
+        }
+    }
 }
