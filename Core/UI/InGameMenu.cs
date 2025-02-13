51f75a6b98b31c4e5589fac1d8623ddc6d72748a
Pavel Škop <pskop.k10@gmail.com>
2025-02-13 01:56:31 +0100

    inventory prep

--- Core/UI/InGameMenu.cs
+++ Core/UI/InGameMenu.cs
@@ -2,53 +2,36 @@
 using Microsoft.Xna.Framework;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework.Input;
-using System;
-
 namespace TBoGV;
 
 internal class InGameMenu : IDraw
 {
-    private static Texture2D spriteBackground;
-    private static Viewport viewport;
-    public bool Active { get; set; }
-    private List<ItemContainer> itemContainers;
-    private const float BackgroundOpacity = 0.25f;
-    private const int ContainerSpacing = 60;
-
+    static Texture2D SpriteBackground;
+    static Viewport Viewport;
+    public bool Active;
+    List<ItemContainer> ItemContainers;
     public InGameMenu(Viewport viewport)
     {
-        InGameMenu.viewport = viewport;
-        spriteBackground = TextureManager.GetTexture("blackSquare");
+        Viewport = viewport;
+        SpriteBackground = TextureManager.GetTexture("blackSquare");
         Active = false;
-        itemContainers = new List<ItemContainer>();
     }
-
     public void Update(Viewport viewport, Player player, MouseState mouseState)
     {
-        InGameMenu.viewport = viewport;
+        Viewport = viewport;
         player.UpdateContainers(mouseState);
-        itemContainers = player.ItemContainers;
+        ItemContainers = player.ItemContainers;
     }
-
     public void Draw(SpriteBatch spriteBatch)
     {
-        spriteBatch.Draw(spriteBackground, new Rectangle(0, 0, viewport.Width, viewport.Height), new Color(0, 0, 0, (int)(255 * BackgroundOpacity)));
-            
-        if (itemContainers == null || itemContainers.Count == 0)
-            return;
-            
-        Vector2 menuCenter = new Vector2(viewport.Width / 2, viewport.Height / 2);
-        int numRows = (int)Math.Ceiling(Math.Sqrt(itemContainers.Count));
-        int numCols = (int)Math.Ceiling((double)itemContainers.Count / numRows);
-        Vector2 startPosition = menuCenter - new Vector2((numCols - 1) * ContainerSpacing / 2, (numRows - 1) * ContainerSpacing / 2);
-
-        for (int i = 0; i < itemContainers.Count; i++)
+        spriteBatch.Draw(SpriteBackground, new Rectangle(0, 0, Viewport.Width, Viewport.Height), new Color(0, 0, 0, (int)(255 * 0.25)));
+        // Draw item containers in the middle of the menu
+        Vector2 menuCenter = new Vector2(GraphicsDeviceManager.DefaultBackBufferWidth / 2, GraphicsDeviceManager.DefaultBackBufferHeight / 2);
+        for (int i = 0; i < ItemContainers.Count; i++)
         {
-            int row = i / numCols;
-            int col = i % numCols;
-            Vector2 containerPosition = startPosition + new Vector2(col * ContainerSpacing, row * ContainerSpacing);
-            itemContainers[i].Position = containerPosition;
-            itemContainers[i].Draw(spriteBatch);
+            Vector2 containerPosition = menuCenter + new Vector2((i - ItemContainers.Count / 2) * 50, 0);
+            ItemContainers[i].Position = containerPosition;
+            ItemContainers[i].Draw(spriteBatch);
         }
     }
 }
